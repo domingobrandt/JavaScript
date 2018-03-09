e044e0323548170c313dd2a3242b7be228cf69b4
domingobrandt <brandtyet02@gmail.com>
2018-03-09 17:19:14 -0400

    arreglo, arrays
    ejemplo de arreglos en javascript arreglo en otro arreglo y llamados

---
+++ arrays.js
@@ -0,0 +1,25 @@
+
+let newYearsResolutions 
+= ['Run a marathon', 'Learn a new language', 'Read 52 books'];
+
+let listItem = newYearsResolutions[0];
+console.log(listItem);
+
+console.log(newYearsResolutions[2]);
+
+console.log(newYearsResolutions[3]);
+
+/*declarar un arreglo cualquiera
+1.
+Individual elements of arrays can also be stored to variables.
+
+Create a variable named listItem and set it equal to the first item in your newYearsResolutions array using square bracket notation ([]).
+
+Then use console.log() to print the listItem variable to the console.
+
+2.
+Now, console.log() the third item in the newYearsResolutions array without using a variable.
+
+3.
+Try to log the item at position [3] to the console. What is logged to the console? 
+arregle dentro de otro arreglo */
