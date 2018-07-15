f2452e676e5ce818eef35a8ed48cab5b34e95619
domingobrandt <brandtyet02@gmail.com>
2018-07-14 23:11:12 -0400

    loops es6
    charAt slice

---
+++ Udacity/for..of..loops.js
@@ -0,0 +1,33 @@
+/*
+ * Programming Quiz: Writing a For...of Loop (1-4)
+JavaScript String charAt() Method
+var str="hello world"
+console.log(str.charAt(0)) 
+https://www.w3schools.com/jsref/jsref_charat.asp
+
+ JavaScript array slice()Method
+https://www.w3schools.com/jsref/jsref_slice_array.asp
+
+Directions:
+Write a for...of loop that:
+
+loops through each day in the days array
+capitalizes the first letter of the day
+and prints the day out to the console
+Your code should log the following to the console:
+
+Sunday
+Monday
+Tuesday
+Wednesday
+Thursday
+Friday
+Saturday
+*/
+
+const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+// your code goes here
+for(const day of days){
+console.log(day.charAt(0).toUpperCase()+ day.slice(1));
+}
