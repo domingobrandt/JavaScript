f031fe3a278a3b66096f5752ae7ebdafd20d7622
domingobrandt <brandtyet02@gmail.com>
2018-07-16 09:37:30 -0400

    JavaScript Array Method
    .shift() .unshift() .slice()

---
+++ CodeCademy/MethodShitUnshitSlice.js
@@ -0,0 +1,22 @@
+/*w3schools.com
+The shift() method removes the first item of an array.
+The unshift() method adds new items to the beginning of an array, and returns the new length.
+The slice() method returns the selected elements in an array, as a new array object.
+The slice() method selects the elements starting at the given start argument, and ends at, but does not include, the given end argument.
+Note: The original array will not be changed.
+*/
+
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+
+groceryList.unshift('popcorn');
+console.log(groceryList);
+
+
+console.log(groceryList.slice(1,2));
+console.log(groceryList);
+
+/*
+console.log(groceryList.slice(1, 4));
+console.log(groceryList);*/
